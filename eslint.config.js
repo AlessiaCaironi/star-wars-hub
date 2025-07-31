e4a48be35b46bd449d006ab459906d12857e7399
Alessia Caironi <alessia.caironi@orobix.com>
2025-07-31 23:45:41 +0200

    feat: Add eslint. Add prettier. Change logo.

--- eslint.config.js
+++ eslint.config.js
@@ -2,22 +2,81 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import tsParser from '@typescript-eslint/parser'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+import react from 'eslint-plugin-react'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import prettierPlugin from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+
+export default [
+  js.configs.recommended,
   {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    ignores: ['dist'],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'jsx-a11y': jsxA11y,
+      'simple-import-sort': simpleImportSort,
+      prettier: prettierPlugin,
+      import: importPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      semi: 'off',
+      'react/require-default-props': 'off',
+      'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/function-component-definition': [
+        'error',
+        { namedComponents: 'arrow-function' },
+      ],
+      'import/extensions': ['warn', 'never'],
+      'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'sort-imports': 'off',
+      'import/order': 'off',
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': 'error',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-shadow': 'off',
+      '@typescript-eslint/no-shadow': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: false,
+          singleQuote: true,
+        },
+      ],
     },
   },
-])
+]
